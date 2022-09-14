f5aa34723209fb751976a7a9d321d8249d09125e
amresh-singh-kipm <amresh.kipm@gmail.com>
2022-09-14 18:21:15 +0530

    a reusable form is created

--- src/frontend/Home.js
+++ src/frontend/Home.js
@@ -2,76 +2,109 @@ import React, { useContext, useEffect, useState } from "react";
 import HighOC from "./HighOC";
 import Navbar from "./Navbar";
 import ParentCallback from "./ParentCallback";
-import {cartContext} from '../index'
+import { cartContext } from "../index";
 import CustomHooks from "./CustomHooks";
-
+import Form from "./Form";
 
 function Home() {
-const products = useContext(cartContext)
+  const formData = [
+    {
+      name: "Name",
+      type: "text",
+      className: { inputClass: "form-control", lableClass: "form-group" },
+    },
+    {
+      name: "Email",
+      type: "email",
+      className: { inputClass: "form-control", lableClass: "form-group" },
+    },
+    {
+      name: "Password",
+      type: "password",
+      className: { inputClass: "form-control", lableClass: "form-group" },
+    },
+    {
+      name: "Confirm-Password",
+      type: "password",
+      className: { inputClass: "form-control", lableClass: "form-group" },
+    },
+    {
+      name: "month",
+      type: "month",
+      className: { inputClass: "form-control", lableClass: "form-group" },
+    },
+  ];
+  const formHeaderClass = "form-section";
 
-const [data,error] = CustomHooks("https://jsonplaceholder.typicode.com/posts")
- console.log("this is custom hook data",data)
+ 
+
+  const products = useContext(cartContext);
+
+  const [data, error] = CustomHooks(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log("this is custom hook data", data);
   const [cartItems, setCartItems] = useState([]);
   console.log("CART ITEMS:", cartItems);
-  console.log("if error",error)
+  console.log("if error", error);
 
-  function cartItem(){
-    localStorage.setItem("cartproduct",JSON.stringify(products))
-    if(!localStorage.getItem("cartValue")){
-      localStorage.setItem("cartValue","[]")
+  function cartItem() {
+    localStorage.setItem("cartproduct", JSON.stringify(products));
+    if (!localStorage.getItem("cartValue")) {
+      localStorage.setItem("cartValue", "[]");
     }
   }
-let cartproducts = JSON.parse(localStorage.getItem("cartproduct"));
-let cartItemValue = JSON.parse(localStorage.getItem("cartValue"));
+  let cartproducts = JSON.parse(localStorage.getItem("cartproduct"));
+  let cartItemValue = JSON.parse(localStorage.getItem("cartValue"));
 
-  function addItemToCart(id){
-    let item = cartproducts.find((item)=>item.id===id)
-    if(cartItemValue.length===0){
-      cartItemValue.push(item)
-    }
-    else{
-     let  resp = cartItemValue.find((product)=>product.id===id)
-      if(resp===undefined){
-        cartItemValue.push(item)
+  function addItemToCart(id) {
+    let item = cartproducts.find((item) => item.id === id);
+    if (cartItemValue.length === 0) {
+      cartItemValue.push(item);
+    } else {
+      let resp = cartItemValue.find((product) => product.id === id);
+      if (resp === undefined) {
+        cartItemValue.push(item);
       }
     }
-    localStorage.setItem("cartValue",JSON.stringify(cartItemValue))
+    localStorage.setItem("cartValue", JSON.stringify(cartItemValue));
   }
-  useEffect(()=>{
-cartItem();
-  },[])
+  useEffect(() => {
+    cartItem();
+  }, []);
 
   return (
     <>
-    <div className="row">
-      <Navbar />
-      {data && data.map((item,i)=>{
+      <div className="row">
+        <Navbar />
+        {/* {data && data.map((item,i)=>{
         
         return(<p key={i}>
         {item.body}
         
         </p>)
-      })}
-      {products &&
-        products.map((item,index) => (
-          <div className="col-lg-4" key={index}>
-            <img src={item.image} alt="Show text if  not shown" />
-            <p className="item-name"  > {item.title} </p>
-            <p className="item-name"> The cost is ${item.price}</p>
-          
-            <button
-              className="btn btn-primary"
-              onClick={()=>addItemToCart(item.id)}>
-              Buy Now
-            </button>
-          
-          </div>
-        ))}
-          </div> <div>
-       <HighOC component={ParentCallback}/>
-       </div>
-       </>
-          
+      })} */}
+        {products &&
+          products.map((item, index) => (
+            <div className="col-lg-4" key={index}>
+              <img src={item.image} alt="Show text if  not shown" />
+              <p className="item-name"> {item.title} </p>
+              <p className="item-name"> The cost is ${item.price}</p>
+
+              <button
+                className="btn btn-primary"
+                onClick={() => addItemToCart(item.id)}
+              >
+                Buy Now
+              </button>
+            </div>
+          ))}
+      </div>{" "}
+      <div>
+        <Form name={formData} formHeaderClass={formHeaderClass} />
+        {/* <HighOC component={ParentCallback}/> */}
+      </div>
+    </>
   );
 }
 
--- src/frontend/Navbar.js
+++ src/frontend/Navbar.js
@@ -1,31 +1,31 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 function Navbar() {
   let nagivate = useNavigate();
   // let item = JSON.parse(localStorage.getItem("name"));
   // const [data,setData] = useState(item)
- 
- 
   return (
     <div className="nav">
-    <div className="nav__center container">
-    <NavLink to="/" className = "login" >Home</NavLink>
-    <NavLink to = "/parentcallback" className="login">Hooks</NavLink> 
-      <div className="nav__logo">
-        <h1>E-<span>CART</span></h1>
-      </div>
-      
-      <ul className="nav__list">
-        <NavLink to="/login" className="login" >Login</NavLink>
-        
-        <div className="cart__icon">
-          <NavLink to="/cart" className="login">cart</NavLink>
-          {/* <span className="item__total">{data?.length}</span> */}
+      <div className="nav__center container">
+        <NavLink to="/" className="login">
+          Home
+        </NavLink>
+        <NavLink to="/parentcallback" className="login">
+          Hooks
+        </NavLink>
+        <div className="nav__logo">
+          <h1>
+            E-<span>CART</span>
+          </h1>
         </div>
-      </ul>
+        <NavLink to="/login" className="login">
+          Login
+        </NavLink>
+        <NavLink to="/cart" className="login">
+          cart
+        </NavLink>
+      </div>
     </div>
-  </div>
-    
   );
 }
 
